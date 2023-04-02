e965faf4d48a5563529f81bea7dd5998edb9c9a9
sempl911 <55957696+sempl911@users.noreply.github.com>
2023-04-02 20:08:53 +0300

    add all

---
+++ DataSaveProject/Assets/Scripts/MainMenu/MenuHandlerUI.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MenuHandlerUI : MonoBehaviour
+{
+    public TMP_InputField user_inputField;
+    public TextMeshProUGUI user_name;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SaveName();
+        }
+    }
+
+    public void setName()
+    {
+        user_name.text  = user_inputField.text ;
+        SaveManager.Instance.userName = user_name.text;
+    }
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+# if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+    public void SaveName()
+    {
+        setName();
+        SaveManager.Instance.userName = user_name.text;
+        SaveManager.Instance.SaveDataPlayer();
+    }
+    public void LoadData()
+    {
+        SaveManager.Instance.LoadDataPlayer();
+    }
+}
---
+++ DataSaveProject/Assets/Scripts/SaveManager/SaveManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SaveManager : MonoBehaviour
+{
+    public static SaveManager Instance;
+    public string userName;
+    public int bestScore;
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public string name;
+        public int bestScore;
+
+    }
+        public void SaveDataPlayer()
+        {
+            SaveData dataName = new SaveData();
+
+            dataName.name = userName;
+            dataName.bestScore = bestScore;
+
+            string json = JsonUtility.ToJson(dataName);
+
+            File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+        Debug.Log("saved");
+        }
+        public void LoadDataPlayer()
+        {
+            string path = Application.persistentDataPath + "/savefile.json";
+            if (File.Exists(path))
+            {
+                string json = File.ReadAllText(path);
+                SaveData data = JsonUtility.FromJson<SaveData>(json);
+                userName = data.name;
+                bestScore = data.bestScore;
+            Debug.Log("Loaded");
+            }
+        }
+    
+}
